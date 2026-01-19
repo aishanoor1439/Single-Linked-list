a4e5c47addf6c3d6ddcacf6359ca85f91a80c985
aishanoor1439 <aisha.noor1439@gmail.com>
2026-01-19 20:36:21 +0500

    First Commit

---
+++ SingleLinkedList/LinkedList.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nodes
+{
+    internal class LinkedList
+    {
+        public Node head;
+
+        public void push(int new_data)
+        {
+            Node new_node = new Node(new_data);
+            new_node.next = head;
+            head = new_node;
+        }
+
+        public void append(int new_data)
+        {
+            Node new_node = new Node(new_data);
+            if (head == null)
+            {
+                head = new Node(new_data);
+                return;
+            }
+            new_node.next = null;
+            Node last = head;
+            while (last.next != null) {
+            last = last.next;
+            }
+            last.next = new_node;
+            new_node.next = null;
+            return;
+        }
+
+        public void insert_after(Node prey_node, int new_data)
+        {
+            if(prey_node == null)
+            {
+                Console.WriteLine("The given previous node cannot be null.");
+                return;
+            }
+            Node new_node = new Node(new_data);
+            new_node.next = prey_node.next;
+            prey_node.next = new_node;
+        }
+
+        public void deleteNode(int key)
+        {
+            if (head == null)
+            {
+                Console.WriteLine("List is empty.");
+                return;
+            }
+            if(head.data == key)
+            {
+                head = head.next;
+                return;
+            }
+            Node temp = head;
+            while(temp.next != null && temp.next.data != key)
+            {
+                temp = temp.next;
+            }
+            if(temp.next == null)
+            {
+                Console.WriteLine("Value not found in the list.");
+                return;
+            }
+            temp.next = temp.next.next;
+        }
+        public void printList()
+        {
+            Node n = head;
+            while (n != null)
+            {
+                Console.WriteLine(n.data + "->");
+                n = n.next;
+            }
+        }
+    }
+}
---
+++ SingleLinkedList/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nodes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.head = new Node(1);
+            Node second = new Node(2);
+            Node third = new Node(3);
+
+            list.head.next = second;
+            second.next = third;
+
+            list.push(23);
+            list.append(20);
+            list.insert_after(second, 26);
+            list.insert_after(third, 23);
+            list.deleteNode(1);
+            list.printList();
+        }
+    }
+}
